fa2311b96a1e0a737b5988e961be6f117643ee1b
Ben Jamin <bromy.co@gmail.com>
2016-12-19 03:41:08 -0500

    add basic dragndrop

--- renderer.js
+++ renderer.js
@@ -6,6 +6,15 @@ const imagemin = require('imagemin')
 const imageminMozjpeg = require('imagemin-mozjpeg')
 const imageminPngquant = require('imagemin-pngquant')
 
+const $$dropTarget = document.querySelector('.drop-target')
+
+$$dropTarget.addEventListener('dragenter', handleDragEnter)
+$$dropTarget.addEventListener('dragleave', handleDragEnter)
+$$dropTarget.addEventListener('dragover', handleDragOver)
+$$dropTarget.addEventListener('drop', handleDrop)
+
+console.log('v0.0.5')
+
 x = document.querySelector('.file-selector')
 
 console.log(__dirname, path)
@@ -27,3 +36,39 @@ x.addEventListener('change', (e) => {
   });
 
 })
+
+function handleDragEnter(e) {
+  e.preventDefault()
+  e.stopPropagation()
+  console.log('drag enter')
+  // this.classList.add('is-drag-entered');
+  return false;
+}
+
+function handleDragLeave(e) {
+  e.preventDefault()
+  e.stopPropagation()
+  console.log('drag leave')
+  // this.classList.remove('is-drag-entered');
+  return false;
+}
+
+function handleDragOver(e) {
+  e.preventDefault()
+  e.stopPropagation()
+  console.log('drag over')
+  return false;
+}
+
+
+function handleDrop(e) {
+  e.preventDefault()
+  e.stopPropagation()
+
+  console.log('drop', e)
+
+  this.classList.remove('is-drag-entered');
+  e.dataTransfer.dropEffect = 'copy';
+
+  return false;
+}
