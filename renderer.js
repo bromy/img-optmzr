17884e756d0cf113d70acffd2c29f395ad988842
Ben Jamin <bromy.co@gmail.com>
2016-12-25 18:41:37 -0500

    compress imgs dropped into target

--- renderer.js
+++ renderer.js
@@ -8,6 +8,12 @@ const imageminPngquant = require('imagemin-pngquant')
 
 const $$dropTarget = document.querySelector('.drop-target')
 
+let options = {
+  quality: {
+    jpeg: 95
+  }
+}
+
 $$dropTarget.addEventListener('dragenter', handleDragEnter)
 $$dropTarget.addEventListener('dragleave', handleDragEnter)
 $$dropTarget.addEventListener('dragover', handleDragOver)
@@ -23,25 +29,49 @@ x.addEventListener('change', (e) => {
 
   let xPath = x.files[0].path;
 
-  console.log(e, x.files[0].path, path.dirname(xPath), path.dirname(xPath) + '\\..\\imgs-optimzed' )
+  console.log(e, x.files[0].path, path.dirname(xPath), path.dirname(xPath) + '\\..\\imgs-optimzed')
 
   imagemin([xPath], path.dirname(xPath) + '\\..\\imgs-optimzed', {
-      plugins: [
-          imageminMozjpeg(),
-          imageminPngquant({quality: '65-80'})
-      ]
+    plugins: [
+      imageminMozjpeg({
+        //quality: options.quality.jpeg
+      }),
+      imageminPngquant({
+        quality: '65-80'
+      })
+    ]
   }).then(files => {
-      console.log(files);
-      //=> [{data: <Buffer 89 50 4e …>, path: 'build/images/foo.jpg'}, …]
+    console.log('Files compressed!', files);
+    //=> [{data: <Buffer 89 50 4e …>, path: 'build/images/foo.jpg'}, …]
   });
 
 })
+
+function optimizeImage(src, dest) {
+
+  console.log('optimizing an image: ', src, dest)
+
+  imagemin([src], path.dirname(src) + path.sep + 'optimized', {
+    plugins: [
+      imageminMozjpeg({
+        quality: options.quality.jpeg
+      }),
+      imageminPngquant({
+        quality: '65-80'
+      })
+    ]
+  }).then(files => {
+    console.log('Files compressed!', files);
+    //=> [{data: <Buffer 89 50 4e …>, path: 'build/images/foo.jpg'}, …]
+  });
+
+}
 
 function handleDragEnter(e) {
   e.preventDefault()
   e.stopPropagation()
   console.log('drag enter')
-  // this.classList.add('is-drag-entered');
+    // this.classList.add('is-drag-entered');
   return false;
 }
 
@@ -49,7 +79,7 @@ function handleDragLeave(e) {
   e.preventDefault()
   e.stopPropagation()
   console.log('drag leave')
-  // this.classList.remove('is-drag-entered');
+    // this.classList.remove('is-drag-entered');
   return false;
 }
 
@@ -60,15 +90,26 @@ function handleDragOver(e) {
   return false;
 }
 
-
 function handleDrop(e) {
   e.preventDefault()
   e.stopPropagation()
 
-  console.log('drop', e)
+  console.log('drop', e.dataTransfer.files)
+
+  const droppedFiles = e.dataTransfer.files
 
   this.classList.remove('is-drag-entered');
   e.dataTransfer.dropEffect = 'copy';
+
+  console.log('Number of files: ', droppedFiles.length)
+
+  for (let i = 0; i < droppedFiles.length; i++) {
+    console.log(droppedFiles[i])
+
+
+    optimizeImage(droppedFiles[i].path, 'nothing')
+
+  }
 
   return false;
 }
